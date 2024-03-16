747d2b015b229fa9720da0d88af94ffe69221584
lilitantonyan <lilit@helix.am>
2024-03-16 18:35:28 +0400

    add pages

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(function (){
+    $('.service__list').slick({
+        centerMode: true,
+        slidesToShow: 4,
+        arrows:false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $('.news__list').slick({
+        centerMode: true,
+        slidesToShow: 4,
+        arrows:false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+})
